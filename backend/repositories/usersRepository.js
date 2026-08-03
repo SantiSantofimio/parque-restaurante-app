c77f430cb1bcd0393dfab5d0872675666e097ab6
Santiago Santofimio <san_andres_santo@hotmail.com>
2026-08-03 10:59:37 +0200

    refactor: migrate users repository to base repository

--- backend/repositories/usersRepository.js
+++ backend/repositories/usersRepository.js
@@ -1,32 +1,17 @@
+import BaseRepository from './BaseRepository.js'
+
 import {
   USERS_FILE,
 } from '../config/paths.js'
 
-import {
-  readJsonFile,
-  writeJsonFile,
-} from '../utils/jsonStorage.js'
+class UsersRepository
+  extends BaseRepository {
 
-class UsersRepository {
+  constructor() {
 
-  getAll() {
-
-    return readJsonFile(
+    super(
       USERS_FILE
     )
-
-  }
-
-  findById(
-    id
-  ) {
-
-    return this
-      .getAll()
-      .find(
-        user =>
-          user.id === id
-      )
 
   }
 
@@ -43,80 +28,10 @@ class UsersRepository {
 
   }
 
-  save(
-    user
-  ) {
-
-    const users =
-      this.getAll()
-
-    users.push(
-      user
-    )
-
-    writeJsonFile(
-      USERS_FILE,
-      users
-    )
-
-    return user
-
-  }
-
-  update(
-    updatedUser
-  ) {
-
-    const users =
-      this.getAll()
-
-    const index =
-      users.findIndex(
-        user =>
-          user.id ===
-          updatedUser.id
-      )
-
-    if (
-      index === -1
-    ) {
-      return null
-    }
-
-    users[index] =
-      updatedUser
-
-    writeJsonFile(
-      USERS_FILE,
-      users
-    )
-
-    return updatedUser
-
-  }
-
-  delete(
-    id
-  ) {
-
-    const users =
-      this.getAll()
-
-    const filtered =
-      users.filter(
-        user =>
-          user.id !== id
-      )
-
-    writeJsonFile(
-      USERS_FILE,
-      filtered
-    )
-
-  }
-
 }
 
-const usersRepository = new UsersRepository()
+const usersRepository =
+  new UsersRepository()
 
-export default usersRepository
+export default
+  usersRepository
