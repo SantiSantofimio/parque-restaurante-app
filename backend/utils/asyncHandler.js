ede0cb3d750a83b03b725aa0a63fe7cb763250c6
Santiago Santofimio <san_andres_santo@hotmail.com>
2026-08-06 21:07:29 +0200

    feat(core): add reusable error handling infrastructure

---
+++ backend/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+function asyncHandler(
+    controller
+) {
+
+    return function (
+        req,
+        res,
+        next
+    ) {
+
+        Promise
+            .resolve(
+                controller(
+                    req,
+                    res,
+                    next
+                )
+            )
+            .catch(
+                next
+            )
+
+    }
+
+}
+
+export default asyncHandler
